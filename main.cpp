21351bb0f0d9c9cb5cfa74c20d52c54ef15cbc15
h00512929 <h00512929>
2019-08-10 13:38:57 +0000

    git init two files

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sat 10 Aug 2019 01:35:14 PM UTC
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    printf("this is the local branch");
+    return 0;
+
+}
---
+++ test.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sat 10 Aug 2019 01:37:23 PM UTC
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+void test(void)
+{
+    printf("this is a source file");
+}
